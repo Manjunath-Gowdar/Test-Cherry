db0cabb19c94b84fdf2f481d7bbd77f67b90ea5b
Manjunath-Gowdar-au25 <manjunathaggowdar@gmail.com>
2022-08-13 18:46:18 +0530

    prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -22,9 +22,7 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json()) //to parse data in req.body
 
-app.get('/', (req, res) => {
-  res.send('API is running')
-})
+
 
 // to send paypal config
 app.get('/api/config/paypal', (req, res) => {
@@ -39,6 +37,19 @@ app.use('/api/upload', uploadRoutes)
 // uploading image
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running....')
+  })
+}
 
 // error handling middleware
 app.use(notFound)
